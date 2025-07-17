b7b052d156177593e5fb364c13bc6a3b815d4f99
Ricardo <ricardo@iMac-de-Ricardo.local>
2025-07-17 14:11:00 -0300

    add shadcn header component

--- src/app/api/pizzas/route.ts
+++ src/app/api/pizzas/route.ts
@@ -2,6 +2,11 @@ import { getAllProducts } from "@/lib/services/product";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const pizzas = await getAllProducts()
+    let pizzas = await getAllProducts()
+    pizzas.map(pizza => ({
+            ...pizza,
+            image: `${process.env.NEXT_PUBLIC_BASE_URL}/pizzas/${pizza.image}`
+        })
+    )
     return NextResponse.json({ pizzas })
 }

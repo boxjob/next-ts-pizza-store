52a8a5aaa9a2a2b08f9334de999a444e0909cf3d
Ricardo <ricardo@iMac-de-Ricardo.local>
2025-07-18 17:57:48 -0300

    add signin signup, validate_email - api route

--- src/app/api/pizzas/route.ts
+++ src/app/api/pizzas/route.ts
@@ -2,7 +2,7 @@ import { getAllProducts } from "@/lib/services/product";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    let pizzas = await getAllProducts()
+    const pizzas = await getAllProducts()
     pizzas.map(pizza => ({
             ...pizza,
             image: `${process.env.NEXT_PUBLIC_BASE_URL}/pizzas/${ pizza.image }` 
